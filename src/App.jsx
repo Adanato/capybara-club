1601b96cb499b9526c1667d10f85c5fadfc10cd6
Adam Nguyen <Adanatonw@gmail.com>
2023-08-02 06:29:02 -0400

    reworked file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 
 // Main Pages
-import Home from "./pages/HomePage";
-import About from "./pages/AboutPage";
-import Capybara from "./pages/CapybaraPage";
-import Auth from "./pages/AuthPage";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Capybara from "./pages/Capybara";
+import Auth from "./pages/auth/Auth";
 
 // Sub Pages
-import Post from "./pages/PostPage";
+import Post from "./pages/post/Post";
 
 // Common component
-import Header from "./shared/Header";
-import Footer from "./shared/Footer";
-import NotFound from "./shared/NotFoundPage";
+import Header from "./shared/header/Header";
+import Footer from "./shared/footer/Footer";
+import NotFound from "./shared/notfound/NotFound";
 
 function App() {
   return (
